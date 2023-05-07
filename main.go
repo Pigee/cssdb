c84a39ceb1e70edbf57ea9690e3c8e2ca75d8cda
pigee <lijp@localhost.localdomain>
2023-05-08 00:07:41 +0800

    basically works

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os/exec"
 )
 
 func main() {
-	str := "mysqldump -u root -pSxxm@123 cs_s_run | mysql -u root -pSxxm@123 cs_s_run_sx"
+	http.HandleFunc("/initdb", orgHandler)
+
+	log.Println("Listening...")
+	log.Fatal(http.ListenAndServe(":8888", nil))
+
+}
+
+func orgHandler(w http.ResponseWriter, r *http.Request) {
+
+	keys, ok := r.URL.Query()["org_no"]
+	org_no := "defaultorgno"
+
+	if ok {
+		org_no = keys[0]
+	}
+
+	str := "mysqldump -u root -pSxxm@123 cs_s_run | mysql -u root -pSxxm@123 cs_s_run_" + org_no
+	log.Println(str)
+
 	output, err := exec.Command("bash", "-c", str).Output()
 	if err != nil {
 		panic(err)
 	}
 	log.Println(string(output))
+
 }
