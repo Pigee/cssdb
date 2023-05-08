f06f07641250e8f6ab5f3f6126a422bc88e06b74
Pigee <haiou511@gmail.com>
2023-05-08 16:22:17 +0800

    working ugly...

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module cssdb
 
 go 1.19
+
+require (
+	github.com/BurntSushi/toml v1.2.1
+	github.com/go-sql-driver/mysql v1.7.1
+)
--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"github.com/BurntSushi/toml"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+        "encoding/json"
 )
 
-func main() {
-	http.HandleFunc("/initdb", orgHandler)
+var infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+var errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	log.Println("Listening...")
-	log.Fatal(http.ListenAndServe(":8888", nil))
+type Cssdbconf struct {
+	Impstr string
+	Dbstr  string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/initdb", orgHandler)
+
+	infoLog.Println("Listening...")
+	errorLog.Fatal(http.ListenAndServe(":8888", mux))
+	// 获取应用配置
+	//myconf := getToml()
+	//DB, _ := sql.Open("mysql", "sxadmin:sx@123@tcp(192.168.199.100:3306)/cs_s_run")
+	//createDb(myconf)
 
 }
 
@@ -18,18 +36,67 @@ func orgHandler(w http.ResponseWriter, r *http.Request) {
 
 	keys, ok := r.URL.Query()["org_no"]
 	org_no := "defaultorgno"
+	myconf := getToml()
 
 	if ok {
 		org_no = keys[0]
 	}
 
-	str := "mysqldump -u root -pSxxm@123 cs_s_run | mysql -u root -pSxxm@123 cs_s_run_" + org_no
-	log.Println(str)
-
+	err := createDb(myconf, org_no)
+        if err != nil {
+               return
+           }
+	str := myconf.Impstr + org_no
+	infoLog.Println(str)
 	output, err := exec.Command("bash", "-c", str).Output()
 	if err != nil {
 		panic(err)
 	}
-	log.Println(string(output))
+	infoLog.Println(string(output))
+
+        w.WriteHeader(http.StatusOK)
+        w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["message"] = "Status OK"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		errorLog.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+        return
+
+}
+
+func getToml() (c Cssdbconf) {
+
+	var conf Cssdbconf
+	if _, err := toml.DecodeFile("./conf/cssdb.toml", &conf); err != nil {
+		// handle error
+		errorLog.Fatal(err)
+	}
+	return conf
+}
+
+func createDb(c Cssdbconf, o string)(e error) {
+	infoLog.Println(c.Dbstr)
+	DB, _ := sql.Open("mysql", c.Dbstr)
+	DB.SetConnMaxLifetime(100)
+	DB.SetMaxIdleConns(10)
+	if err := DB.Ping(); err != nil {
+		infoLog.Println("open database fail")
+		return err
+	}
+	infoLog.Println("connnect cs_s_run database success...")
+
+	sqlStr := "create database cs_s_run_" + o + " charset utf8mb4"
+	ret, err := DB.Exec(sqlStr)
+	if err != nil {
+		errorLog.Println("create database failed ,err:%v\n", err)
+		return err
+	} else {
+		i, _ := ret.RowsAffected()
+		infoLog.Println("i: %v\n", i)
+	}
+       return
 
 }
